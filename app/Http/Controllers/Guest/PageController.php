e09b0cd4cc74d3f20fa742688191444392c950f0
DaveCorleto <davide.corleto@fastwebnet.it>
2024-02-13 18:32:08 +0100

    Film in pagina!

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,9 +5,17 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// importo il Model
+
+use App\Models\Movie;
+
 class PageController extends Controller
-{
+{   
+    // Questo è un metodo index per restituire la vista della pagina index 
     public function index () {
-        return view('pages.index'); 
+        // mi salvo tutto (all) quello che mi arriva da Movies nella variabile $movies e lo uso nel Controller
+        // con il compact    
+        $movies = Movie :: all();
+        return view('pages.index', compact('movies')); 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', [PageController :: class, 'index']);
+
